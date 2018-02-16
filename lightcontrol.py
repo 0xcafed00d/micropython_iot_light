6a1843a503da653e3049f041609e7938bebd8d76
Lee Witek <neomech@gmail.com>
2018-02-16 09:58:06 +0000

    added countdown timer

--- lightcontrol.py
+++ lightcontrol.py
@@ -1,5 +1,32 @@
 import machine
 import neopixel
+import utime
+
+
+class CountdownTimer:
+    def __init__(self, time_ms):
+        self.reset(time_ms)
+
+    def reset(self, time_ms):
+        self.duration_ms = time_ms
+        self.start_time = utime.ticks_ms()
+        self.expired = False
+
+    def elapsedTime(self):
+        if self.expired:
+            return self.duration_ms
+        return utime.ticks_diff(utime.ticks_ms(), self.start_time)
+
+    def hasExpired(self):
+        if not self.expired:
+            self.expired = self.elapsedTime() > self.duration_ms
+        return self.expired
+
+    def getProgress(self):
+        if self.expired:
+            return 1.0
+        return float(self.elapsedTime()) / float(self.duration_ms)
+
 
 np = neopixel.NeoPixel(machine.Pin(13), 17)
 
