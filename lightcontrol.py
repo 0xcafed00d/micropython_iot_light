23dfdc661e5167e47e74a689196b93afa76cdc46
Lee Witek <neomech@gmail.com>
2018-02-18 12:48:48 +0000

    replace mode flags with function

--- lightcontrol.py
+++ lightcontrol.py
@@ -34,55 +34,38 @@ class LightControl:
         self.timer = CountdownTimer(0)
         self.begin_rgb = (0, 0, 0)
         self.end_rgb = (0, 0, 0)
-        self.discoMode = False
-        self.randomMode = False
-        self.phases = [0.0, 0.0, 0.0]
-        self.rates = [0.1, 0.2, 0.3]
+        self.doFunc = None
 
     def setColourAll(self, colour):
         self.np.fill(colour)
         self.np.write()
 
     def tranistionTo(self, colour, time_ms):
-        self.discoMode = False
-        self.randomMode = False
+        self.doFunc = self.doFade
         self.begin_rgb = self.end_rgb
         self.end_rgb = colour
         self.timer.reset(time_ms)
 
     def goDisco(self):
-        self.setColourAll((0, 0, 0))
-        self.discoMode = True
-        self.randomMode = False
+        self.doFunc = self.doDisco
         self.begin_rgb = (0, 0, 0)
         self.end_rgb = (0, 0, 0)
 
     def goRandom(self):
-        self.setColourAll((0, 0, 0))
-        self.discoMode = False
-        self.randomMode = True
+        self.doFunc = self.doRandom
         self.begin_rgb = (0, 0, 0)
         self.end_rgb = colours[urandom.randint(0, 6)]
         self.timer.reset(1000)
 
     def doDisco(self):
-        for p in range(0, 16):
-            r = abs(math.sin(math.pi/16 * p + self.phases[0])) * 255
-            g = abs(math.sin(math.pi/16 * p + self.phases[1])) * 255
-            b = abs(math.sin(math.pi/16 * p + self.phases[2])) * 255
-
-            self.phases[0] += 0.01
-            self.phases[1] += 0.001
-            self.phases[2] += 0.03
-
-            self.np[p] = (int(r), int(g), int(b))
-
-        self.np.write()
+        pass
 
     def doFade(self):
         newrgb = lerpRGB(self.begin_rgb, self.end_rgb,
                          self.timer.getProgress())
         self.setColourAll(newrgb)
+        if self.timer.hasExpired():
+            self.doFunc = None
 
     def doRandom(self):
         newrgb = lerpRGB(self.begin_rgb, self.end_rgb,
@@ -94,9 +77,5 @@ class LightControl:
             self.timer.reset(500)
 
     def doLightControl(self):
-        if self.discoMode:
-            self.doDisco()
-        elif self.randomMode:
-            self.doRandom()
-        else:
-            self.doFade()
+        if self.doFunc is not None:
+            self.doFunc()
