068a2201b782f0d62a31da18aaeaed17469911f4
Lee Witek <neomech@gmail.com>
2018-03-09 23:19:37 +0000

    use onboard led as wifi connection indicator

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import network
-import time
+import utime
+import machine
 
+from timeout import Timeout
 import wifi_config
+
+led = machine.Pin(5, machine.Pin.OUT)
+led.value(1)
+
 
 net = network.WLAN(network.STA_IF)
 net.active(True)
 net.connect(wifi_config.wifi_ssid, wifi_config.wifi_pw)
 
+connectTimeout = Timeout(10000)
+
 while not net.isconnected():
-    time.sleep_ms(100)
+    led.value(not led.value())
+    utime.sleep_ms(100)
+    if connectTimeout.hasExpired():
+        machine.reset()
+
+
+led.value(0)
 
 print(net.ifconfig())
 
